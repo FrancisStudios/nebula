7394d84b69fe3545dd81218b4aa787bf40e03959
unknown <vlfmap@gmail.com>
2023-12-02 16:10:55 +0100

    player controls

--- src/player-controls.gd
+++ src/player-controls.gd
@@ -28,28 +28,7 @@ func _input(event):
 		print(result)
 		
 		navigationAgent.target_position = result.position
-		
-#func _physics_process(delta):
-#	velocity = Vector3.ZERO
-#	var direction
-# 
-#	if Input.is_action_just_pressed('click'):
-#		mouse_pos = get_viewport().get_mouse_position()
-#
-#		var mouse_pos_3D: Vector3 = Vector3(mouse_pos.x, 0, mouse_pos.y)
-#		direction = (mouse_pos_3D - position).normalized()
-#		look_at(direction)
-#		velocity = (direction * speed)
-#
-#	if direction:
-#		velocity.x = direction.x * speed
-#		velocity.z = direction.z * speed
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, speed)
-#		velocity.z = move_toward(velocity.z, 0, speed)
-#
-#
-#	move_and_slide()
+	
 
 func _process(delta):
 	if navigationAgent.is_navigation_finished():
